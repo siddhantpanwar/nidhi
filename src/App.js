67aa500ebb31df5b0e269897ba0ca1e5a32c8eb4
siddhantpanwar <60474913+siddhantpanwar@users.noreply.github.com>
2022-11-06 22:32:14 +0530

    last

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="main">
+      <div className="img">
+        <h2>What YOu Doin</h2>
+      </div>
+      <div className="img2">
+        <h3>don't u miss me</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
